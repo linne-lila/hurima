a32b87e71ff7e08f78bddd0008161440bc926c72
linne <linne7351@gmail.com>
2026-02-07 22:12:24 +0900

    2-7

---
+++ src/resources/views/catalog.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>catalog</title>
+
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/catalog.css') }}" />
+</head>
+
+<body>
+    @include('part.header')
+    
+</body>
+</html>
